2bb92612aece2a7f5516564e2facfab3c5392dcf
Gia Henville <140769584+giabui@users.noreply.github.com>
2023-10-03 13:58:12 -0400

    feat: Create grid

---
+++ script.js
@@ -0,0 +1,21 @@
+const gridContainer = document.getElementById("gridContainer");
+
+function createCells(size){
+    gridContainer.style.setProperty("--grid-rows", size);
+    gridContainer.style.setProperty("--grid-cols", size);
+
+    //makes sure that grid size is always 700px
+    gridContainer.style.setProperty("--grid-cell-size", getGridCellSize(size));
+
+    for(let i = 0; i < (size * size); i++){
+        let cell = document.createElement("div");
+        cell.innerText = i + 1;
+        cell.classList.add("cell");
+        gridContainer.appendChild(cell);
+    };
+};
+createCells(16);
+
+function getGridCellSize(size){
+    return 700 / size + "px";
+}
